0e1c5e8ca044701f0430831c33217acc0cb861a9
Jana <yanix2x2@gmail.com>
2025-06-19 11:44:11 +0300

    add path to json

--- render_website.py
+++ render_website.py
@@ -1,16 +1,15 @@
 import os
 import json
 import math
+import argparse
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
 from more_itertools import chunked
-
-with open('media/meta_data.json', 'r', encoding="UTF-8") as meta_data:
-    books_json = meta_data.read()
+from dotenv import load_dotenv
 
 
-def on_reload():
+def on_reload(books):
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
@@ -18,15 +17,14 @@ def on_reload():
 
     template = env.get_template('template.html')
 
-    books = json.loads(books_json)
-
     os.makedirs('pages', exist_ok=True)
 
     books_per_page = 20
+    columns = 2
     pages = list(chunked(books, books_per_page))
     page_amount = math.ceil(len(books) / books_per_page)
     for page_num, books_chunk in enumerate(pages, start=1):
-        page = list(chunked(books_chunk, 2))
+        page = list(chunked(books_chunk, columns))
         rendered_page = template.render(
             books=page,
             page_number=page_num,
@@ -38,9 +36,27 @@ def on_reload():
             file.write(rendered_page)
 
 
-server = Server()
-server.watch('template.html', on_reload)
-server.serve(root='.')
+def main():
+    load_dotenv()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-p',
+        '--path', 
+        help='Путь до json файла', 
+        default=os.getenv('BOOKS_PATH', 'media/meta_data.json')
+    )
+    args = parser.parse_args()
+
+    with open(args.path, 'r', encoding="UTF-8") as file:
+        books = json.load(file)
+
+    on_reload(books)
+
+    server = Server()
+    server.watch('template.html', on_reload)
+    server.serve(root='.')
+
 
 if __name__ == '__main__':
-    on_reload()
+    main()
